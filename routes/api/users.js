0f286c724b04853ec6202fbcca62bcd395a1b2c4
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2020-12-13 15:27:28 +0600

    Worked on User Registration and password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 // route POST api/users
 // Register user
 router.post(
@@ -13,13 +17,45 @@ router.post(
       "Please enter password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     //for when error occurs
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+    const { name, email, password } = req.body;
+
+    try {
+      //See if user already exists
+
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        res.send(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      //Get user gravatr
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //Encrypt Password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send("User registerd");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
