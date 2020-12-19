813d6e24c9406adbe62e0c9b5caeb70f6e728727
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2020-12-19 22:38:56 +0600

    Get Current User Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// route GET api/profile
-// this is test toute
-router.get("/", (req, res) => res.send("Profile route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// route GET api/profile/me
+// Get current user profile
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
