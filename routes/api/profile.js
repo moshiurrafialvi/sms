c9ffe00c688a64f169efd5e5d28b04c2a53960e8
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-02-04 23:42:24 +0600

    Get all  profiles and Profile By User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,39 @@ router.post(
   }
 );
 
+// @route    GET api/profile
+// @desc     GET all profiles
+// @access   Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     GET profile by user id
+// @access   Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile)
+      return res.status(400).json({ msg: "No profile found for this user." });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "No profile found for this user." });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
