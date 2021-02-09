bb4ea4eaaa6981affbb2b216412667aec0d1cee8
hissan07 <khairatunhissan@iut-dhaka.edu>
2021-02-09 15:01:30 +0600

    Profile

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import './App.css';
@@ -49,6 +50,7 @@ const App=() => {
      <PrivateRoute exact path='/add-education' component={AddEducation}/>
      <PrivateRoute exact path="/edit-profile" component={EditProfile} />
      <Route exact path='/profiles' component={Profiles}/>
+     <Route exact path='/profile/:id' component={Profile}/>
      <PrivateRoute exact path='/posts' component={Posts}/>
      <PrivateRoute exact path='/posts/:id' component={Post}/>
    </switch>
